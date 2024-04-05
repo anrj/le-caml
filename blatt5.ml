194a941d3a59fa9395ea9de68e5eed928bee69bb
Anri Javakhishvili <ktnmstr@pm.me>
2024-04-05 04:12:13 +0400

    🐫 Week01 exercises exc. Longest Twins

--- blatt5.ml
+++ blatt5.ml
@@ -28,12 +28,27 @@ match db with
 | st::sts -> if st.semester = sem then aux sem sts (acc + 1) else aux sem sts acc
 in aux sem db 0
 
+(*Not dividing by count / not average but total*)
 let rec student_avg_grade : int -> database -> float = fun id db ->
 match db with
 | [] -> 0.0
 | st::sts -> if st.id = id then List.fold_left (fun acc (_, g) -> acc +. g) 0.0 st.grades else student_avg_grade id sts
 
-let course_avg_grade : int -> database -> float = fun 
+let course_avg_grade : int -> database -> float = fun crs db ->
+let rec aux crs db acc count =
+match db with
+| [] -> if count = 0 then 0.0 else acc /. float_of_int count
+| st::sts -> 
+    let rec check_grades grades acc count =
+    match grades with
+    | [] -> (acc, count)
+    | (c, g)::t ->
+        if c = crs
+        then check_grades t (acc +. g) (count + 1)
+        else check_grades t acc count
+in let (acc', count') = check_grades st.grades acc count
+in aux crs sts acc' count'
+in aux crs db 0.0 0
 
 (*List Mishmash*)
 let rec interleave3 lst1 lst2 lst3 = match lst1 with    
@@ -62,7 +77,7 @@ let rec aux x c acc = match c with
 in
 aux x (List.rev coefficients) 0.
 
-let derive_poly coefficients =
+(*let derive_poly coefficients =
 let rec aux c = match c with
 | [] -> ([], -1)
 | c::cs -> let (result, degree) = aux c in
@@ -70,9 +85,10 @@ let rec aux c = match c with
     else ((c *. (float_of_int (degree + 1)))::result, degree + 1)
 in
 let (result, _) = aux c in result
-
+*)
 let rec derive_poly2 = function
-| [] | [h] -> []  
+| [] -> [] 
+| [h] -> []  
 | h::t -> (h *. (float_of_int (List.length t))) :: derive_poly2 t
  
 (*¯\_(ツ)_/¯*)
